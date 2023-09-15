b970f42bd911f373aa916c1e44098598311ee9f1
Elena2485 <elena2485@yandex.ru>
2023-09-15 17:04:23 +0300

    demoqa_home

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-
+from selenium.common.exceptions import NoSuchElementException
 @pytest.fixture(scope='session')
 def browser():
     driver = webdriver.Chrome
@@ -18,13 +18,25 @@ class SwagLabs(BasePage):
         self.driver = driver
         self.base_url = 'https://www.saucedemo.com/'
 
+ # 6/ перейти на страницу https: // www.saucedemo.com /
+ # проверить наличие иконки
+
     def visit(self):
         return self.driver.get(self.base_url)
-    click_on_the_icon()
+        return self.click_on_the_icon(self.base_url)
 
+# 7 в файле test_check_swag.py реализуйте следующий тест кейс:
+    # перейти на страницу https://www.saucedemo.com/
+    # проверить наличие поля имени
     def find_element(self, locator):
-        time.sleep(3)  # задержка на 3 секунды
-        return self.driver.find_element(By.CSS_SELECTOR, locator)
+        return self.driver.find_element('div.login_logo', locator)
 
-    def get_url(self):
-        return self.driver.current_url
+# 7 / перейти на страницу https://www.saucedemo.com/
+# проверить наличие поля пароля
+
+    def get_pass(self):
+#return self.driver.get(self.base_url)
+        if self.find_element('input#password.input_error.form_input') <> 0
+            return True
+         return False
+
