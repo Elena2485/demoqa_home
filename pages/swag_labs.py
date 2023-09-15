fcd1fb950d030316d10240ba2223859b67cfa596
Elena2485 <elena2485@yandex.ru>
2023-09-15 04:37:46 +0300

    demoqa_home

---
+++ pages/swag_labs.py
@@ -0,0 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+from pages.base_page import BasePage
+class SwagLabs(BasePage):
+
+
+    def exist_icon(self):
+        try:
+            self.find_element(locator='div.login_logo')
+        except NoSuchElementException:
+            return False
+        return True
+
+    def click_on_the_icon(self):
+        self.find_element(locator='div.login_logo').click()
+
+    def equal_url(self):
+        if self.get_url() == 'https://www.saucedemo.com/':
+            return True
+        return False
+
+
---
+++ tests/test_check_swag.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope='session')
+def browser():
+    driver = webdriver.Chrome
+    yield driver
+    driver.quit()
+
+############a.	в файле test_check_swag.py реализуйте следующий тест кейс:
+#перейти на страницу https://www.saucedemo.com/
+#проверить наличие иконки
+
+from pages.base_page import BasePage
+class SwagLabs(BasePage):
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_url = 'https://www.saucedemo.com/'
+
+    def visit(self):
+        return self.driver.get(self.base_url)
+    click_on_the_icon()
+
+    def find_element(self, locator):
+        time.sleep(3)  # задержка на 3 секунды
+        return self.driver.find_element(By.CSS_SELECTOR, locator)
+
+    def get_url(self):
+        return self.driver.current_url
